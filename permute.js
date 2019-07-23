99889e074ce0499c806ec99366c0c07c7b8134b1
YCKJ1943 <kanghaizhen@cloudwalk.cn>
2019-07-23 11:10:27 +0800

    新增排序算法

--- permute.js
+++ permute.js
@@ -28,3 +28,25 @@ let insert = function(num, arr){
   }
   return result;
 }
+
+var permute_2 = function(nums){
+  if(nums && nums.length < 2){
+    return [nums]
+  }
+  let result = [];
+  backtrack(nums, result, [])
+  return result;
+}
+
+var backtrack = function(nums, result, arr){
+  if(nums.length == 0){
+    result.push(arr)
+  } else {
+    for(let i = 0; i < nums.length; i++){
+      let newArr = [...arr, nums[i]]
+      let newNums = [...nums]
+      newNums.splice(i,1)
+      backtrack(newNums, result, newArr)
+    }
+  }
+}
